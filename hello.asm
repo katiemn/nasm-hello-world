c2aa119f2ece2ca7e51a1c48c2d97b501a305432
katiemn <katiemn@gmail.com>
2018-03-22 13:32:52 -0700

    Add files via upload

---
+++ hello.asm
@@ -0,0 +1,16 @@
+          global    start
+
+          section   .text
+start:
+          mov       rax, 0x2000004
+          mov       rdi, 1
+          mov       rsi, message
+          mov       rdx, 17
+          syscall
+
+          mov       rax, 0x2000001
+          mov       rdi, 0
+          syscall
+
+          section   .data
+message:  db        '最近怎么样?', 10
